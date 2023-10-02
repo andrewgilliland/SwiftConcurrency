bb5bde93e112a6a6d32cc1309cf697db1041dc72
Andrew Gilliland <andrewpgilliland@gmail.com>
2023-10-02 10:25:07 -0500

    feat: updates docatchtrythrowsview

--- SwiftConcurrency/DoTryCatchThrowsView.swift
+++ SwiftConcurrency/DoTryCatchThrowsView.swift
@@ -1,8 +1,34 @@
 import SwiftUI
 
+class DoCatchTryThrowsDataManager {
+    
+    func getTitle() -> String {
+        return "Title from data manager."
+    }
+}
+
+class DoCatchTryThrowsViewModel: ObservableObject {
+    
+    @Published var text: String = "Starting text from view model."
+    let manager = DoCatchTryThrowsDataManager()
+    
+    func fetchTitle() {
+        let newTitle = manager.getTitle()
+        self.text = newTitle
+    }
+}
+
 struct DoTryCatchThrowsView: View {
+    
+    @StateObject private var viewModel = DoCatchTryThrowsViewModel()
+    
     var body: some View {
-        Text("Do, Try, Catch, Throws")
+        Text(viewModel.text)
+            .frame(width:300, height: 300)
+            .background(.blue)
+            .onTapGesture {
+                viewModel.fetchTitle()
+            }
     }
 }
 
